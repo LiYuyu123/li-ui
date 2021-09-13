23117cd51967a228c004cc5642e58d49a2f33762
LiYu <2248868140@qq.com>
2021-09-13 21:00:47 +0800

    支持level属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,10 +1,48 @@
 <template>
   <div>
     <h1>实例1</h1>
-    <Button>你好 </Button>
-    <Button theme="button">你好 </Button>
-    <Button theme="link">你好 </Button>
-    <Button theme="text">你好 </Button>
+    <div>
+      <Button>你好 </Button>
+      <Button theme="button">你好 </Button>
+      <Button theme="link">你好 </Button>
+      <Button theme="text">你好 </Button>
+    </div>
+    <h1>实例2</h1>
+    <div>
+      <div>
+        <Button size="big">大大 </Button>
+        <Button>普通 </Button>
+        <Button size="small">小小 </Button>
+      </div>
+      <div>
+        <Button theme="link" size="big">大大</Button>
+        <Button theme="link">普通</Button>
+        <Button size="small" theme="link">小小</Button>
+      </div>
+      <div>
+        <Button size="big" theme="text">大大</Button>
+        <Button theme="text">普通</Button>
+        <Button size="small" theme="text">小小</Button>
+      </div>
+    </div>
+    <h1>示例3</h1>
+    <div>
+      <div>
+        <Button level="main">主要按钮</Button>
+        <Button>普通按钮</Button>
+        <Button level="danger">危险按钮</Button>
+      </div>
+      <div>
+        <Button theme="link" level="main">主要链接按钮</Button>
+        <Button theme="link">普通链接按钮</Button>
+        <Button theme="link" level="danger">危险链接按钮</Button>
+      </div>
+      <div>
+        <Button theme="text" level="main">主要文字按钮</Button>
+        <Button theme="text">普通文字按钮</Button>
+        <Button theme="text" level="danger">危险文字按钮</Button>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
