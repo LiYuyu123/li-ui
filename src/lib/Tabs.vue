7a5c44afcfbd013cd26a12f2fcaeb00a92a9feab
LiYu <2248868140@qq.com>
2021-09-14 19:13:17 +0800

    完成Tabs和Tab组件之间的嵌套插槽

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
-    1111111111
+    <div v-for="(i,index) in titles " :key="index" >{{i}}</div>
+   <component  v-for="(t,index) in defaults"  :is="t" :key="index" />
   </div>
 </template>
+<script lang="ts">
+import Tab from '../lib/Tab.vue'
+export default {
+  components:{Tab},
+  setup(props,context){
+    const defaults=context.slots.default()
+    defaults.forEach(i=>{
+      if(i.type!==Tab){
+        throw new Error('Tabs 子标签必须是 Tab')
+      }
+    })
+  const titles= defaults.map(tab=>{
+      return tab.props.title
+    })
+    return {defaults,titles}
+  }
+}
+</script>
