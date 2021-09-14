00c5aad5a8b0d9b50e804aa2fb576acf2875677f
LiYu <2248868140@qq.com>
2021-09-14 16:15:44 +0800

    使用Teleport

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,19 +1,21 @@
 <template>
   <template v-if="visible">
-    <div class="gulu-dialog-overlay" @click="onClickOverlay"></div>
-    <div class="gulu-dialog-wrapper">
-      <div class="gulu-dialog">
-        <header><slot name="title"/><span @click="onClick" class="gulu-dialog-close"></span>
-        </header>
-        <main>
-          <slot name="content"/>
-        </main>
-        <footer>
-          <Button @click="cancel">取消</Button>
-          <Button @click="ok" level="main">确定</Button>
-        </footer>
+    <Teleport to="body">
+      <div class="gulu-dialog-overlay" @click="onClickOverlay"></div>
+      <div class="gulu-dialog-wrapper">
+        <div class="gulu-dialog">
+          <header><slot name="title"/><span @click="onClick" class="gulu-dialog-close"></span>
+          </header>
+          <main>
+            <slot name="content"/>
+          </main>
+          <footer>
+            <Button @click="cancel">取消</Button>
+            <Button @click="ok" level="main">确定</Button>
+          </footer>
+        </div>
       </div>
-    </div>
+    </Teleport>
   </template>
 </template>
 <script lang="ts">
