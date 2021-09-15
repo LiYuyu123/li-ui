1b274cafd653e8b5ae5c04c32f048b1ae6c3932a
LiYu <2248868140@qq.com>
2021-09-15 17:00:58 +0800

    引入github markdown 样式

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -6,28 +6,28 @@
         <h2>文档</h2>
         <ol>
           <li>
-            <router-link to="/doc/intro">介绍</router-link>
+            <router-link to="/doc/intro" active-class="selected">介绍</router-link>
           </li>
           <li>
-            <router-link to="/doc/install">安装</router-link>
+            <router-link to="/doc/install"  active-class="selected">安装</router-link>
           </li>
           <li>
-            <router-link to="/doc/get-start">开始使用</router-link>
+            <router-link to="/doc/get-start"  active-class="selected">开始使用</router-link>
           </li>
         </ol>
         <h2>组件列表</h2>
         <ol>
           <li>
-            <router-link to="/doc/switch">Switch 组件</router-link>
+            <router-link to="/doc/switch"  active-class="selected">Switch 组件</router-link>
           </li>
           <li>
-            <router-link to="/doc/button">Button 组件</router-link>
+            <router-link to="/doc/button"  active-class="selected">Button 组件</router-link>
           </li>
           <li>
-            <router-link to="/doc/dialog">Dialog 组件</router-link>
+            <router-link to="/doc/dialog"  active-class="selected">Dialog 组件</router-link>
           </li>
           <li>
-            <router-link to="/doc/tabs">Tabs 组件</router-link>
+            <router-link to="/doc/tabs"  active-class="selected">Tabs 组件</router-link>
           </li>
         </ol>
       </aside>
@@ -79,8 +79,8 @@ export default {
 }
 aside {
   background: lightblue;
+  padding: 16px 0;
   width: 150px;
-  padding: 16px;
   position: fixed;
   top: 0;
   left: 0;
@@ -88,10 +88,20 @@ aside {
   height: 100%;
   > h2 {
     margin-bottom: 4px;
+    padding: 0 16px;
   }
   > ol {
     > li {
-      padding: 4px 0;
+
+      >a{
+        display: block;
+        padding: 4px 16px;
+        text-decoration: none;
+        &.selected{
+          background: white;
+        }
+      }
+
     }
   }
   @media(max-width: 500px) {
