0421da5da3486815b5b1e30067076782f2a48b4a
LiYu <2248868140@qq.com>
2021-09-15 15:48:41 +0800

    完成feature的样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,15 +12,29 @@
     </div>
   </div>
   <div class="feature">
-    <svg class="icon" >
-      <use xlink:href="#icon-Vue"></use>
-    </svg>
-    <svg class="icon" >
-      <use xlink:href="#icon-typescript"></use>
-    </svg>
-    <svg class="icon" >
-      <use xlink:href="#icon-light"></use>
-    </svg>
+    <ul>
+      <li>
+        <svg class="icon" >
+          <use xlink:href="#icon-Vue"></use>
+        </svg>
+        <h3>基于 Vue 3</h3>
+        <p>使用了 Vue 3 Composition API</p>
+      </li>
+      <li>
+        <svg class="icon" >
+          <use xlink:href="#icon-typescript"></use>
+        </svg>
+        <h3>基于 TypeScript</h3>
+        <p>源代码采用 TypeScript 书写</p>
+      </li>
+      <li>
+        <svg class="icon" >
+          <use xlink:href="#icon-light"></use>
+        </svg>
+        <h3>代码易读</h3>
+        <p>每个组件的源代码都极其简洁</p>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -33,9 +47,41 @@ export default {
 
 <style lang="scss" scoped>
 .feature{
-  >svg{
-    width: 64px;
-    height:64px;
+  margin: 64px auto;
+  width: 400px;
+  @media (min-width: 800px) {
+    width: 800px;
+  }
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+  >ul {
+    display: flex;
+    flex-wrap: wrap;
+    >li {
+      width: 400px;
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+           'icon  title'
+           'icon  text';
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+      >svg {
+        grid-area: icon;
+        width: 64px;
+        height: 64px;
+      }
+      >h3 {
+       grid-area: title;
+        font-size: 28px;
+      }
+      >p {
+        grid-area: text;
+      }
+    }
   }
 }
 .topNavAndBanner{
@@ -43,6 +89,8 @@ export default {
   background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
   clip-path: ellipse(80% 60% at 50% 40%);
 }
+
+
 .banner {
   padding: 100px 0;
   display: flex;
