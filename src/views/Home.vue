84f49f19ce6b60e6f7dbfdbfc7e939531ecccdcc
LiYu <2248868140@qq.com>
2021-09-16 19:23:57 +0800

    创建index.ts和rollup.config.js文件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
       <h1>Li UI</h1>
       <h2>一个厉害的UI框架</h2>
       <p class="actions">
-        <a href="https://github.com">GitHub</a>
+        <a href="https://github.com/LiYuyu123/li-ui">GitHub</a>
         <router-link to="/doc">开始</router-link>
       </p>
     </div>
